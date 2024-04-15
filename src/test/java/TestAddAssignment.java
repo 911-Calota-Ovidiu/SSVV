e06020d1eb1602230abbe011f68a0b0c211d1697
911-Calota-Ovidiu <ovidiu.calota@stud.ubbcluj.ro>
2024-04-15 15:36:49 +0300

    WBT tests done

--- src/test/java/TestAddAssignment.java
+++ src/test/java/TestAddAssignment.java
@@ -22,18 +22,69 @@ public class TestAddAssignment {
         TemaXMLRepository fileRepository2 = new TemaXMLRepository(temaValidator, "temetest.xml");
         NotaXMLRepository fileRepository3 = new NotaXMLRepository(notaValidator, "notetest.xml");
         service = new Service(fileRepository1, fileRepository2, fileRepository3);
-        UI console = new UI(service);
     }
 
     @Test
     void testAddValidAssignment(){
-        int result = service.saveTema("1", "Description", 14, 1);
+        int result = service.saveTema("1", "Description of a valid tema(for testing purposes only, studends ignore)", 14, 1);
         Assertions.assertEquals(result,0);
     }
 
     @Test
-    void testAddInvalidAssignment(){
-        int result = service.saveTema("1", null, 14, 1);
+    void testAddInvalidIdAssignment1(){
+        int result = service.saveTema("", "Description", 14, 1);
+        Assertions.assertEquals(result,1);
+    }
+    @Test
+    void testAddInvalidIdAssignment2(){
+        int result = service.saveTema(null, "Description", 14, 1);
+        Assertions.assertEquals(result,1);
+    }
+    @Test
+    void testAddInvalidDescriptionAssignment1(){
+        int result = service.saveTema("2", "", 14, 1);
+        Assertions.assertEquals(result,1);
+    }
+
+    @Test
+    void testAddInvalidDescriptionAssignment2(){
+        int result = service.saveTema("3", null, 14, 1);
+        Assertions.assertEquals(result,1);
+    }
+
+    @Test
+    void testAddInvalidStartlineAssignment1(){
+        int result = service.saveTema("4", "Description", 14, 0);
+        Assertions.assertEquals(result,1);
+    }
+
+    @Test
+    void testAddInvalidStartlineAssignment2(){
+        int result = service.saveTema("5", "Description", 4, 123);
+        Assertions.assertEquals(result,1);
+    }
+
+    @Test
+    void testAddInvalidStartlineAssignment3(){
+        int result = service.saveTema("6", "Description", 2, 3);
+        Assertions.assertEquals(result,1);
+    }
+
+    @Test
+    void testAddInvalidDeadlineAssignment1(){
+        int result = service.saveTema("7", "Description", 0, 3);
+        Assertions.assertEquals(result,1);
+    }
+
+    @Test
+    void testAddInvalidDeadlineAssignment2(){
+        int result = service.saveTema("8", "Description", 15, 3);
+        Assertions.assertEquals(result,1);
+    }
+
+    @Test
+    void testAddInvalidDeadlineAssignment3(){
+        int result = service.saveTema("9", "Description", 2, 3);
         Assertions.assertEquals(result,1);
     }
 }
